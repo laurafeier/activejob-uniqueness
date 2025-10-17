0bac311edf3f9ed4cbbea79e466f674f701ab531
Phil Phillips <phil.phillips@kodehealth.com>
2025-10-17 14:25:34 -0500

    feat: support sidekiq 8
    Wrapper class names for Active Job support have changed in Sidekiq 8.
    https://github.com/sidekiq/sidekiq/blob/main/Changes.md#800
    > WARNING The underlying class name for Active Jobs has changed from ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper to Sidekiq::ActiveJob::Wrapper. The old name will still work in 8.x.
    
    Adding Sidekiq::ActiveJob::Wrapper to the class check in the sidekiq patch.

--- lib/active_job/uniqueness/sidekiq_patch.rb
+++ lib/active_job/uniqueness/sidekiq_patch.rb
@@ -6,7 +6,9 @@ require 'sidekiq/api'
 module ActiveJob
   module Uniqueness
     def self.unlock_sidekiq_job!(job_data)
-      return unless job_data['class'] == 'ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper' # non ActiveJob jobs
+      unless %w[ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper Sidekiq::ActiveJob::Wrapper].include?(job_data['class'])
+        return
+      end
 
       job = ActiveJob::Base.deserialize(job_data.fetch('args').first)
 
